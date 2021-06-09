069a8bcbb92bd69fd70c12260d595f374b4466dd
Gisha <gishathedev@gmail.com>
2021-06-09 14:21:53 +0300

    add basic movement

---
+++ Killbox/Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Gisha.Killbox.Core
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Core")]
+        [SerializeField] private Joystick joystick;
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed = 10f;
+
+        public Vector3 MoveInput => new Vector3(joystick.Horizontal, 0f, joystick.Vertical).normalized;
+
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (MoveInput.magnitude > 0)
+                transform.rotation = Quaternion.LookRotation(MoveInput);
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = MoveInput * moveSpeed;
+        }
+    }
+}
